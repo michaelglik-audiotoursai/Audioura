29dfb932215b3e0a35d3faa707ebb488a6f6614f
michael.glik@gmail.com <michael.glik@gmail.com>
2026-05-24 12:32:32 -0400

    fix: import dart:async in my_tours_screen.dart (unawaited build break)

--- audio_tour_app/lib/screens/my_tours_screen.dart
+++ audio_tour_app/lib/screens/my_tours_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
